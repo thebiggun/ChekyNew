f7d5e083b1ea22e177a520914e395c77d375f2e3
Yatish Agarwal <96855947+thebiggun@users.noreply.github.com>
2025-03-22 09:34:22 +0530

    Update Inventory.jsx

--- Frontend/src/Components/Inventory.jsx
+++ Frontend/src/Components/Inventory.jsx
@@ -8,7 +8,7 @@ const Inventory = () => {
     const [quantity, setQuantity] = useState("");
 
     useEffect(() => {
-        fetch("https://checky.onrender.com/api/getInventory")
+        fetch("http://localhost5000/api/getInventory")
             .then((response) => response.json())
             .then((data) => setInventory(data))
             .catch((error) => console.error("Error fetching inventory:", error));
